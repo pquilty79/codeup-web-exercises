f488acca130150df5be804a12ea129ab105547eb
Patrick Quilty <patrickquilty@Patricks-MacBook-Pro.local>
2021-12-08 11:59:02 -0600

    new files and remove duplicate

--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -1,2 +1,8 @@
-console.log("Node is working!");
-console.log("hello, sweet asteroid!");
+// console.log("Node is working!");
+// console.log("hello, sweet asteroid!");
+// console.log("One of my favorite foods is " + favoriteFood + " They are best when they are fried very crispy. I love to make different sauces to you can have them so many different ways." + " My wife and daughter like them milder than I do, but I prefer them extra hot!")
+
+var favoriteFood = "buffalo wings";
+var phraseOne = "My favorite food is ";
+console.log(phraseOne + favoriteFood)
+
