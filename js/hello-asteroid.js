30ce8770f8684f8e75a3a027a3d14818d9c8a489
Patrick Quilty <patrickquilty@Patricks-MacBook-Pro.local>
2021-12-08 12:29:42 -0600

    add new file

--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -3,6 +3,7 @@
 // console.log("One of my favorite foods is " + favoriteFood + " They are best when they are fried very crispy. I love to make different sauces to you can have them so many different ways." + " My wife and daughter like them milder than I do, but I prefer them extra hot!")
 
 var favoriteFood = "buffalo wings";
-var phraseOne = "My favorite food is ";
+var phraseOne = "\tMy \n\t\tfavorite \n\t\t\tfood \n\t\t\t\tis \n\t\t\t\t\t";
 console.log(phraseOne + favoriteFood)
 
+// "\t" creates a tab and "\n" creates a new line
