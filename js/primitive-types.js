dc377c9f4808b995e9756b42490e6a82917ab745
Patrick Quilty <patrickquilty@Patricks-MacBook-Pro.local>
2021-12-09 08:25:22 -0600

    add homework

---
+++ js/primitive-types.js
@@ -0,0 +1,20 @@
+// 99.9
+//
+// "false"
+//
+// false
+//
+// '0'
+//
+// 0
+//
+// true
+//
+// 'true'
+console.log(typeof 99.9);
+console.log(typeof "false");
+console.log(typeof false);
+console.log(typeof '0');
+console.log(typeof 0);
+console.log(typeof true);
+console.log('true');
