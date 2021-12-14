b28015a11a11b164ae6c508aa97fc2d84e634bf8
Patrick Quilty <patrickquilty@Patricks-MacBook-Pro.local>
2021-12-14 12:56:14 -0600

    changed analyse output to console

--- js/analyse_input.js
+++ js/analyse_input.js
@@ -3,26 +3,26 @@
     var userInput = prompt("Give me a number or a string");
      if (isNaN(parseInt(userInput)) === false) {
          if (userInput % 2 === 0) {
-             alert(userInput + " is an even number.")
+             console.log(userInput + " is an even number.")
             } else {
-             alert(userInput + " is an odd number.")
+             console.log(userInput + " is an odd number.")
          } if (userInput > 0) {
-             alert(userInput + " is a positive number")
+             console.log(userInput + " is a positive number")
             } else if (userInput < 0) {
-             alert(userInput + " is a negative number")
+             console.log(userInput + " is a negative number")
             } else {
-             alert("Zero is neither positive nor negative")
+             console.log("Zero is neither positive nor negative")
          }
      } else {
          if (userInput.length === 1) {
              const vowels = ["a", "e", "i", "o", "u"];
              if (vowels.includes(userInput)) {
-                 alert(userInput + " is a vowel.")
+                 console.log(userInput + " is a vowel.")
              } else {
-                 alert(userInput + " is a consonant.")
+                 console.log(userInput + " is a consonant.")
              }
          } else {
-             alert(userInput + " is " + userInput.length + " characters long");
+             console.log(userInput + " is " + userInput.length + " characters long");
          }
      }
  };
