16aed91995af1dc2e304e463ce8a786d88dca15b
Patrick Quilty <patrickquilty@MacBook-Pro.local>
2021-12-20 10:01:20 -0600

    copleted object exercises

--- js/objects.js
+++ js/objects.js
@@ -11,13 +11,13 @@
      *  > console.log(person.firstName) // "Rick"
      *  > console.log(person.lastName) // "Sanchez"
      */
-    var names  = {
-        firstName: "Patrick",
-        middleName: "William",
-        lastName: "Quilty"
-    };
-    console.log(names.firstName);
-    console.log(names.lastName);
+    // var names  = {
+    //     firstName: "Patrick",
+    //     middleName: "William",
+    //     lastName: "Quilty"
+    // };
+    // console.log(names.firstName);
+    // console.log(names.lastName);
 
     /**
      * TODO:
@@ -28,10 +28,10 @@
      * Example
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
-    names.sayHello = function () {
-        return("Hello, from " + names.firstName + " " + names.lastName + "!");
-    }
-    console.log(names.sayHello());
+    // names.sayHello = function () {
+    //     return("Hello, from " + names.firstName + " " + names.lastName + "!");
+    // }
+    // console.log(names.sayHello());
 
     /** TODO:
      * HEB has an offer for the shoppers that buy products amounting to
@@ -47,11 +47,23 @@
      * and console.log the relevant messages for each person
      */
 
-var shoppers = [
-    {name: 'Cameron', amount: 180},
-    {name: 'Ryan', amount: 250},
-    {name: 'George', amount: 320}
-];
+// var shoppers = [
+//     {name: 'Cameron', amount: 180},
+//     {name: 'Ryan', amount: 250},
+//     {name: 'George', amount: 320}
+// ];
+//
+// shoppers.forEach(function(shopper) {
+//         if (shopper.amount > 200) {
+//             shopper.discount = (shopper.amount * .88);
+//             shopper.discount = shopper.discount.toFixed(2)
+//             console.log(shopper.name + " owes a total of $" + shopper.discount + ", this shopper received as 12% discount.");
+//         } else {
+//             shopper.amount = shopper.amount.toFixed(2)
+//             console.log(shopper.name + " owes a total of $" + shopper.amount + ", this shopper did not receive a discount.");
+//         }
+//     });
+
 
 
     /** TODO:
@@ -66,6 +78,86 @@ var shoppers = [
      * > console.log(books[0].author.firstName) // "Douglas"
      * > console.log(books[0].author.lastName) // "Adams"
      */
+    var books = [
+        {
+            title: "Salmon of Doubt",
+            author: {
+                firstName: "Douglas",
+                lastName: "Adams"
+            }
+        },
+        {
+            title: "The Hitchhiker's Guide to the Galaxy",
+            author: {
+                firstName: "Douglas",
+                lastName: "Adams"
+            }
+        },
+        {
+        title: "Life, the Universe, and Everything",
+        author: {
+        firstName: "Douglas",
+            lastName: "Adams"
+            }
+        },
+        {
+            title: "The Restaurant at the End of the Universe",
+            author: {
+                firstName: "Douglas",
+                lastName: "Adams"
+            }
+        },
+        {
+            title: "So Long and Thanks for All the Fish!",
+            author: {
+                firstName: "Douglas",
+                lastName: "Adams"
+            }
+        },
+        {
+            title: "Mostly Harmless",
+            author: {
+                firstName: "Douglas",
+                lastName: "Adams"
+            }
+        },
+        {
+            title: "Dirk Gently's Holistic Detective Agency",
+            author: {
+                firstName: "Douglas",
+                lastName: "Adams"
+            }
+        },
+        {
+            title: "The Long Dark Tea-Time of the Soul",
+            author: {
+                firstName: "Douglas",
+                lastName: "Adams"
+            }
+        },
+        {
+            title: "Last Chance to See",
+            author: {
+                firstName: "Douglas",
+                lastName: "Adams"
+            }
+        },
+        {
+            title: "The Meaning of Liff",
+            author: {
+                firstName: "Douglas",
+                lastName: "Adams"
+            }
+        },
+        {
+            title: "The Deeper meaning of Liff",
+            author: {
+                firstName: "Douglas",
+                lastName: "Adams"
+            }
+        },
+    ];
+
 
     /**
      * TODO:
@@ -92,6 +184,11 @@ var shoppers = [
      *      ...
      */
 
+    books.forEach(function (book){
+        console.log("Book # " + (books.indexOf(book) + 1));
+        console.log("Title: " + book.title);
+        console.log("Author: " + book.author.firstName + " " + book.author.lastName);
+    })
     /**
      * Bonus:
      * - Create a function named `createBook` that accepts a title and author
@@ -102,5 +199,7 @@ var shoppers = [
      *   outputs the information described above. Refactor your loop to use your
      *   `showBookInfo` function.
      */
+
+
 
 })();
