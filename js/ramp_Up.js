ba730182b381ffc9cbe6e26dde68a0c09953506d
Patrick Quilty <patrickquilty@MacBook-Pro.local>
2022-01-03 12:49:08 -0600

    updated ramp_Up exercises

--- js/ramp_Up.js
+++ js/ramp_Up.js
@@ -29,14 +29,50 @@
 // What if getFizzBuzz doesn't return a value (VOID)?
 // How do you want to handle that?
 
-(function getFizzBuzz(startingNum) {
-    if (startingNum % 3 === 0 && startingNum % 5 === 0 ) {
-        return("FIZZ BUZZ")
-    } else if (startingNum % 3 === 0 && startingNum % 5 !== 0) {
-        console.log("fizz")
-    } else if (startingNum % 3 !== 0 && startingNum % 5 === 0) {
-        console.log("buzz")
-    } else {
-        console.log("Fizbin the Fabulous")
+// function getFizzBuzz(startingNum) {
+//     if (startingNum % 3 === 0 && startingNum % 5 === 0 ) {
+//         return("FIZZ BUZZ")
+//     } else if (startingNum % 3 === 0 && startingNum % 5 !== 0) {
+//         console.log("fizz")
+//     } else if (startingNum % 3 !== 0 && startingNum % 5 === 0) {
+//         console.log("buzz")
+//     } else {
+//         console.log("This number is not evenly divisible by 3 or 5. Give it to Fizban the Fabulous")
+//     }
+// };
+
+// console.log(getFizzBuzz(15))
+// getFizzBuzz(9)
+// getFizzBuzz(10)
+// getFizzBuzz(11)
+
+// function tryGetFizzBuzz(input) {
+//     let maybeFizzBuzzIDK = getFizzBuzz(input);
+//     if (!!maybeFizzBuzzIDK) {
+//         console.log(maybeFizzBuzzIDK)
+//     }
+// }
+//
+// function runThatProgramThing(){
+//     tryGetFizzBuzz(9)
+//     tryGetFizzBuzz(10)
+//     tryGetFizzBuzz(15)
+//     tryGetFizzBuzz(11)
+// }
+// runThatProgramThing()
+
+// Define a function named getPassword() which performs the following actions:
+//     Using prompt(), alert(), variables, and a while-loop,
+//     ask the user to input their password until their entry matches your hard-coded password value.
+//     HINT: start by defining the correct password and don't forget to link your html file
+
+
+(function getPassword() {
+    let password = "jump"
+    let userEntry = prompt("Enter your password");
+    while (userEntry !== password) {
+        alert("Incorrect Password");
+        userEntry = prompt("Enter your password")
     }
-})(11);
+        alert("Success!")
+})();
