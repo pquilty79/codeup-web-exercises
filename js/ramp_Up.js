411a6fb6aa4b2353a6f4a3c5e4fb1827440ec114
Patrick Quilty <patrickquilty@MacBook-Pro.local>
2022-01-03 16:47:04 -0600

    added to rampup

--- js/ramp_Up.js
+++ js/ramp_Up.js
@@ -67,12 +67,47 @@
 //     HINT: start by defining the correct password and don't forget to link your html file
 
 
-(function getPassword() {
-    let password = "jump"
-    let userEntry = prompt("Enter your password");
-    while (userEntry !== password) {
-        alert("Incorrect Password");
-        userEntry = prompt("Enter your password")
-    }
-        alert("Success!")
-})();
+// (function getPassword() {
+//     let password = "mybologneseisawesome"
+//     let userEntry = prompt("Enter your password");
+//     while (userEntry !== password) {
+//         alert("Incorrect Password");
+//         userEntry = prompt("Enter your password")
+//     }
+//         alert("Success!")
+// })();
+//
+// Write a function named getMeaningOfLife which accepts one parameter, meaningfulNum.
+//     This function will use a while loop to compare its current number (a variable named counter) against the meaningfulNum.
+//     While the counter is not equivalent to the meaningfulNum, continue the loop.
+//     Once that condition is broken, console log a string telling the user "The meaning of life, the universe,
+//     and everything is [your meaningful number]"
+// Thinking about the above function, getMeaningOfLife, let’s ask a question: What happens if the
+// value of meaningfulNum is a non-numeric?
+//     Write some code to account for this case. How the function handles the situation is up to you!
+//     Let’s continue with getMeaningOfLife: If the value of meaningfulNum happens to be 42, console log a
+//     separate message at the end of the function to say: "Did you bring your towel?"
+
+// function getMeaningOfLife (meaningfulNum) {
+//     let counter = 0
+//     meaningfulNum = parseInt(meaningfulNum)
+//     if (isNaN(meaningfulNum) || !isFinite(meaningfulNum)) {
+//         console.log("This is not a meaningful number. Perhaps it is the Meaning of Liff, but not the meaning of Life")
+//     } else if (meaningfulNum === 42) {
+//         console.log("The meaning of life, the universe, and everything is " + meaningfulNum)
+//         console.log("Did you bring your towel? Are you sure you know what the question is? Let's build a bigger supercomputer to figure out the question!")
+//     }
+//     else {
+//         while (meaningfulNum !== counter) {
+//             counter++
+//         }
+//         console.log("The meaning of life, the universe, and everything is " + meaningfulNum)
+//     }
+// }
+// getMeaningOfLife(4)
+// getMeaningOfLife(42)
+// getMeaningOfLife("Bob")
+// getMeaningOfLife(Infinity)
+// getMeaningOfLife("42")
+// getMeaningOfLife(false)
+// getMeaningOfLife(-1)
