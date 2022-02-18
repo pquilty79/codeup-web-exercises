fdcd8dcc3483ad8ec97cbea981953a01de2a7077
Patrick Quilty <pquilty79@gmail.com>
2022-02-18 10:56:01 -0600

    added to promises

--- js/promises.js
+++ js/promises.js
@@ -1,7 +1,26 @@
-const githubPromise = fetch('https://api.github.com/repos/pquilty79/codeup-web-exercises/commits',{headers: {'Authorization': `token ${MY_GITHUB_TOKEN}`}} )
-    .then(response => response.json())
-    .catch(error => console.error(error));
-    console.log(githubPromise)
+let username = ""
+function getUserName() {
+    $('#submit').click(function () {
+        username = $('#username').val()
+        getPromise(username)
+    })
+}
+getUserName()
 
+function getPromise(username) {
+    const githubPromise = fetch(`https://api.github.com/repos/${username}/codeup-web-exercises/commits/main`, {headers: {'Authorization': `token ${MY_GITHUB_TOKEN}`}})
+        .then(response => response.json())
+        .then(response => {
+            let date = response.commit.author.date;
+            console.log(convertDate(date))
+        })
+        .catch(error => console.error(error));
+    console.log(githubPromise)
+}
+
+function convertDate (date_to_convert_str) {
+        let date_to_convert = new Date(date_to_convert_str);
+        return date_to_convert.toString();
+    }
 
 
