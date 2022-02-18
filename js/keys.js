791e7fbba7900b3492176bf071f4d667411de15f
Glitch (thunder-outrageous-polka) <none>
2022-02-18 03:29:20 +0000

    🥵🟨 Updated with Glitch

--- js/keys.js
+++
@@ -1,3 +0,0 @@
-const myMapBoxToken = 'pk.eyJ1IjoicHF1aWx0eTc5IiwiYSI6ImNremViOGQ1ZzJ4ZmEycXBybmltNXZpZXYifQ.ih6bW4G_fdzM51bK2DR7Pw';
-const myWeatherToken = '93b4df06c611bc6673e04f3eddcfffa2';
-const MY_GITHUB_TOKEN = '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';
