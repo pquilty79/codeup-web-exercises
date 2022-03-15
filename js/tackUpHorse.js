d4c24091922050cb541a6bf6f7ec5a95ec120e83
Patrick Quilty <pquilty79@gmail.com>
2022-03-15 13:50:37 -0500

    changed to relative path

---
+++ js/tackUpHorse.js
@@ -0,0 +1,18 @@
+function tackUpHorse(horse) {
+    console.log("First: Catch " + horse);
+    console.log("Second: Put halter on " + horse);
+    console.log("Third: Bring back " + horse);
+    console.log("Fourth: Tie up " + horse);
+    console.log("Fifth: Brush down " + horse);
+    console.log("Sixth: Use hoof pick on " + horse);
+    console.log("Seventh: Put blanket on " + horse);
+    console.log("Eight: Put saddle on " + horse);
+    console.log("Ninth: Put on girth ");
+    console.log("Tenth: put bridle on " + horse);
+    console.log("Untie horse and you are ready to mount!");
+}
+tackUpHorse("Pistol");
+tackUpHorse("Little Man");
+tackUpHorse("Raven");
+tackUpHorse("White Beauty");
+tackUpHorse("Black Beauty");
